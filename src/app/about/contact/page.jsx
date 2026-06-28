d02b60d3231aa2bc4d43b0c819809baa80ae2b12
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-06-28 10:39:49 +0600

    first submit

---
+++ src/app/about/contact/page.jsx
@@ -0,0 +1,11 @@
+
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This is Contact Page</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
---
+++ src/app/about/designers/page.jsx
@@ -0,0 +1,11 @@
+
+
+const DesignersPage = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>This is Designers Page</h2>
+        </div>
+    );
+};
+
+export default DesignersPage;
