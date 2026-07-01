e674669e5cfed900eb82bd7139f60ca6dcd8f0d0
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-07-01 11:20:24 +0600

    image optimization 8

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,24 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import tshirt from '../../assets/images/tshirt.jpeg'
+
 const AboutPage = () => {
     return (
         <div>
             <h2>This is About page</h2>
+            <Image width="500" height="300" src="/photo1.png" alt="text photo"></Image>
+            <Image width="500" height="300" src={tshirt} alt="T-Shirt image"></Image>
+            <Image width="500" height="300" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb" alt="silence river view"></Image>
+            <Image width="500" height="300" src="https://i.ibb.co.com/JR1JcDqn/pexels-pixabay-531880.jpg" alt="my soul"></Image>
         </div>
     );
 };
